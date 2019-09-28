5083acc9861904ff76b54ada5b6bfd89231893ab
rafalhawelka@gmail.com <rafalhawelka@gmail.com>
2019-09-28 14:13:33 +0200

    add razor pages taks to github

---
+++ Models/Movie.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [StringLength(60,MinimumLength = 3)]
+        [Required]
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Release Data")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Range(typeof(DateTime), "1/1/1966", "1/1/2020")]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
+        [StringLength(30)]
+        public string Genre { get; set; }
+
+        [Range(1,100)]
+        [Required]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
+        public string Rating { get; set; }
+    }
+}
